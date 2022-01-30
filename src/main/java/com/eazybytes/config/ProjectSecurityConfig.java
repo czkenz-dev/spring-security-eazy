cace6485be4e788b36b59a229032f6650c80b095
CzKenz Dev <czkenz.dev@gmail.com>
2022-01-30 17:15:27 +0700

    Inmemory authen

--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.eazybytes.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,17 +21,22 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        /*http
-                .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/myAccount").authenticated()
                 .antMatchers("/myBalance").authenticated()
                 .antMatchers("/myLoans").authenticated()
                 .antMatchers("/myCards").authenticated()
                 .antMatchers("/notices").permitAll()
                 .antMatchers("/contact").permitAll()
-                .and()
-                .formLogin().and().httpBasic();*/
+                .and().formLogin()
+                .and().httpBasic();
+    }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("12345").authorities("admin").and().
+                withUser("user").password("12345").authorities("read").and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
 }
