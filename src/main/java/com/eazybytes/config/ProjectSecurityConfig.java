3e5fa68472a260884f6b2796b4d5de27164e5021
CzKenz Dev <czkenz.dev@gmail.com>
2022-01-30 16:58:38 +0700

    first commit

---
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.eazybytes.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    /**
+     * /myAccount - Secured
+     * /myBalance - Secured
+     * /myLoans - Secured
+     * /myCards - Secured
+     * /notices - Not Secured
+     * /contact - Not Secured
+     */
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        /*http
+                .authorizeRequests()
+                .antMatchers("/myAccount").authenticated()
+                .antMatchers("/myBalance").authenticated()
+                .antMatchers("/myLoans").authenticated()
+                .antMatchers("/myCards").authenticated()
+                .antMatchers("/notices").permitAll()
+                .antMatchers("/contact").permitAll()
+                .and()
+                .formLogin().and().httpBasic();*/
+
+    }
+
+}
